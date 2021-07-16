ada34a1ca6a48926f494a9d6cfcce5255a40299c
AHG <ahakangenc92@gmail.com>
2021-07-16 17:16:16 -0500

    Commit all components and assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import BackgroundImage from './assets/bg.png';
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Wrapper>
+      <Sidebar />
+      <Main />
+      </Wrapper>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  background: #eefcff; 
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`
+
+const Wrapper = styled.div`
+  background-image: url(${BackgroundImage});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 100%;
+  display: flex;
+`
 
 export default App;
